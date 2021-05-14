38d880e42c807295b9f4104a837349aa3d168a3c
Rahul <rahulbehera2011@gmail.com>
2021-05-15 03:05:14 +0530

    Added boilerplate code

---
+++ app.js
@@ -0,0 +1,7 @@
+const problem = require('./problem/problem') 
+
+
+// Problem: 
+
+// Calling the solution
+problem.solution('args')
---
+++ problem/problem.js
@@ -0,0 +1,9 @@
+// Problem:
+
+const solution = function(args) {
+    return args
+};
+
+module.exports = {
+    solution
+};
