19a4ae0494eed46626afe3353382e3861aec6221
Rahul <rahulbehera2011@gmail.com>
2021-05-15 02:48:31 +0530

    Added solution

---
+++ countString.js
@@ -0,0 +1,7 @@
+// Problem: Write a function which takes in a string and returns counts of each character in the string.
+
+function countStringCharacter(str){
+    return str.length
+}
+
+console.log(countStringCharacter("asfas asd asdas d asd ashi"));
